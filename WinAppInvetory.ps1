e77f4445aa08a898e676a91d1f1d0b62e6f57d51
CigDangle <31552562+cigdangle@users.noreply.github.com>
2023-11-27 17:09:11 -0600

    Add files via upload

---
+++ WinAppInvetory.ps1
@@ -0,0 +1,10 @@
+﻿# Set Variable for Export File Path and Name - Defaults to Desktop
+$InvFile = [Environment]::GetFolderPath("Desktop") + "\SoftwareInventory.csv"
+# Looks at list of files that can be uninstalled as shown in Add/Remove Programs
+$InstalledApps = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+# Grab the the Display name, verison, and installation date of each app
+$List = foreach($obj in $InstalledApps){$obj.GetValue('DisplayName') + "," + $obj.GetValue('DisplayVersion') + "," + $obj.GetValue('InstallDate')}
+# Dump info to Export File
+$List | Out-File -FilePath $InvFile
+# Dump info to screen
+Write-Host $List
